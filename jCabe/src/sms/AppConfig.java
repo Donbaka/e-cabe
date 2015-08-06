0c9d82e555bb7d358044f7db63ef024207b2f3cb
Oka Suryanegara <okasurya@users.noreply.github.com>
2015-08-06 18:26:30 +0700

    sms sender: done

---
+++ jCabe/src/sms/AppConfig.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sms;
+
+/**
+ *
+ * @author brlnt-super
+ */
+public class AppConfig {
+    public static final String GAMMU_BIN = "\"C:\\\\Program Files (x86)\\Gammu 1.33.0\\bin";
+    public static final String GAMMU_CONF_DIR = "\"D:\\#project\\e-cabe\\sms\\conf";
+    
+    public static final String GAMMU = GAMMU_BIN+"\\gammu\"";
+    public static final String GAMMU_SMS = GAMMU_BIN+"\\gammu-smsd\"";
+    public static final String GAMMU_SMS_INJECT = GAMMU_BIN+"\\gammu-smsd-inject\"";
+    public static final String GAMMU_SMS_MONITOR = GAMMU_BIN+"\\gammu-smsd-monitor\"";
+    
+    public static final String GAMMU_CONF = GAMMU_CONF_DIR+"\\gammurc\"";
+    public static final String GAMMU_SMS_CONF = GAMMU_CONF_DIR+"\\smsdrc\"";
+}
